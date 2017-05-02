e46752ae9be7fbd17073db3f108eb5ae6295c311
researchbizagi <researchbizagi@outlook.com>
2017-05-02 17:20:01 -0500

    first setup

---
+++ dojo4angular.server/Controllers/ItemsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.Mvc;
+
+namespace dojo4angular.server.Controllers
+{
+    [Route("api")]
+    public class ItemsController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        [Route("items")]
+        public IEnumerable<string> GetItems()
+        {
+            return new string[] { "apple", "pineapple", "watermelon" };
+        }
+        
+    }
+}

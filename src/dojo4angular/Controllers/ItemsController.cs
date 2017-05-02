933e9676069b5d17e64ebe71b89fc91d7edf8f00
researchbizagi <researchbizagi@outlook.com>
2017-05-02 16:49:41 -0500

    DiegoP: Base service added in .net core

---
+++ src/dojo4angular/Controllers/ItemsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace dojo4angular.Controllers
+{
+    [Route("api")]
+    public class ItemsController : Controller
+    {
+        [Route("items")]
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "apple", "watermelon", "pineapple" };
+        }
+        
+    }
+}
